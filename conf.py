076529d684c52f73dd3d8672c3bfb3a5e18a242b
ABD-01 <63636498+ABD-01@users.noreply.github.com>
2024-09-01 14:48:40 +0530

    Added favicon

--- conf.py
+++ conf.py
@@ -264,6 +264,7 @@ exclude_patterns = [
     ".github/*",
     ".history",
     "github_submodule/*",
+    "README.md",
     "LICENSE.md",
     '_build', 'Thumbs.db', '.DS_Store',
 ]
@@ -330,7 +331,7 @@ html_title = "ABD"
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = "old/assets/img/icon/favicon.ico"
+html_favicon = "favicon.ico"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
