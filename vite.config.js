51b1220ab3fbe8dfc2658b6b2d6158f0ae3eaf31
ABD-01 <git@github.com:ABD-01/ABD-01.github.io.git>
2026-01-20 21:02:21 +0530

    Added github workflow

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     ],
     base: './', // Ensures assets are linked relatively for GitHub Pages
     build: {
-        outDir: '../dist-v5', // Output to a specific folder outside portfolio-v5 for easy deployment
+        outDir: 'dist',
         emptyOutDir: true,
     }
 })
